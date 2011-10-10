30abc960f593fda37b25aa6e463931d984213989
Kevin Malakoff <xmann.intl@gmail.com>
2011-10-10 12:41:14 +0200

    Added Underscore-Awesomer example using _.parseJSON and emulating a couchdb model.

--- ex_underscore_awesomer_js/build/test.js
+++ ex_underscore_awesomer_js/build/test.js
@@ -6,5 +6,49 @@ $(document).ready(function() {
     _.VERSION;
     return _.AWESOMENESS;
   });
-  return test("TODO", function() {});
+  return test("CouchDB an _.parseJSON", function() {
+    var SomeModel, json, some_model_instance;
+    _.PARSE_JSON_TYPE_FIELD = 'type';
+    root.Constructors || (root.Constructors = {});
+    _.PARSE_JSON_CONSTRUCTOR_ROOT = root.Constructors;
+    SomeModel = (function() {
+      var toJSON;
+      function SomeModel(key, value) {
+        this.key = key;
+        this.value = value;
+      }
+      toJSON = function() {
+        return {
+          type: 'some_model',
+          key: this.key,
+          value: this.value
+        };
+      };
+      SomeModel.parseJSON = function(json) {
+        if (json.type !== 'some_model') {
+          throw new Error('unexpected type');
+        }
+        return new SomeModel(json.key, json.value);
+      };
+      return SomeModel;
+    })();
+    root.Constructors.some_model = SomeModel;
+    json = {
+      type: 'some_model',
+      key: 'abcdef',
+      value: {
+        x: 1,
+        y: 2,
+        z: 3
+      }
+    };
+    some_model_instance = _.parseJSON(json);
+    ok(some_model_instance instanceof SomeModel, 'some model was deserialized');
+    equal(some_model_instance.key, 'abcdef', 'key was deserialized');
+    return ok(_.isEqual(some_model_instance.value, {
+      x: 1,
+      y: 2,
+      z: 3
+    }), 'value was deserialized');
+  });
 });
--- ex_underscore_awesomer_js/test.coffee
+++ ex_underscore_awesomer_js/test.coffee
@@ -5,9 +5,32 @@ $(document).ready( ->
     _.VERSION; _.AWESOMENESS
   )
 
-  test("TODO", ->
+  test("CouchDB an _.parseJSON", ->
+    # set up for CouchDB 'type' convention
+    _.PARSE_JSON_TYPE_FIELD = 'type'
+    # make a Constructors namespace known to Underscore
+    root.Constructors||root.Constructors={};
+    _.PARSE_JSON_CONSTRUCTOR_ROOT = root.Constructors
+
+    class SomeModel
+      constructor: (@key, @value) ->
+      toJSON = -> return { type:'some_model', key:this.key, value:this.value }
+      @parseJSON = (json) ->
+        throw new Error('unexpected type') if (json.type!='some_model')
+        return new SomeModel(json.key, json.value)
+    root.Constructors.some_model = SomeModel # add constructor to Constructors namespace
+
+    # assume this comes back from the server
+    json = {type:'some_model', key:'abcdef', value: {x:1, y:2, z:3} }
+
+    # deserialize - _.parseJSON finds 'root.Constructors.some_model'
+    some_model_instance = _.parseJSON(json)
+
     #################################
     # Validating the example
     #################################
+    ok(some_model_instance instanceof SomeModel, 'some model was deserialized')
+    equal(some_model_instance.key, 'abcdef', 'key was deserialized')
+    ok(_.isEqual(some_model_instance.value, {x:1, y:2, z:3}), 'value was deserialized')
   )
 )
