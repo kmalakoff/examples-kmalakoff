8d1a1c1eab0390106a1a04dd41ae85f34d637766
Kevin Malakoff <xmann.intl@gmail.com>
2012-06-15 20:23:15 +0900

    Update test projects to use '_name' instead of 'name_dev'. Updated npm versions from 'latest' to specific versions.

--- ex_client_server/client/vendor/scripts/kmalakoff-shim.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  // unpack some globals for the application
-  if (!this._) {this._ = require('underscore');}
-  if (!this.Backbone) {this.Backbone = require('backbone');}
-  if (!this.Backbone.Articulation) {this.Backbone.Articulation = require('backbone-articulation');}
-  if (!this.ko) {this.ko = require('knockout');}
-  if (!this.kbi) {this.kbi = require('knockback-inspector');}
-})(this);
